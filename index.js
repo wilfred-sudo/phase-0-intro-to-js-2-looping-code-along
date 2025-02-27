534fcefb757379bcba11a51c104ced5be9b6e273
wilfredkiprop <kipropwilfred90@gmail.com>
2025-02-27 18:55:49 +0300

    loop lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let n = 0; n < names.length; n++){
+        messages.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
